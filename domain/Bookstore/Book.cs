5b16583ca8b20921dcda7f77079569a4ed5bbf76
howuvebeenkeeping <rigabdullin@yandex.ru>
2020-07-24 16:27:18 +0500

    Describe the entity book

---
+++ domain/Bookstore/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bookstore
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+        public Book(int id, string title)
+        {
+            Title = title;
+            Id = id;
+        }
+    }
+}
--- domain/Bookstore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Bookstore
-{
-    public class Class1
-    {
-    }
-}
