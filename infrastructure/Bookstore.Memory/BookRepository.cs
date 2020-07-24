a5ccb612b0466bcc682931d162139fc56e72e94f
howuvebeenkeeping <rigabdullin@yandex.ru>
2020-07-24 20:06:54 +0500

    Add implemention BookRepository

--- infrastructure/Bookstore.Memory/BookRepository.cs
+++ infrastructure/Bookstore.Memory/BookRepository.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace Bookstore.Memory
 {
     public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = 
+        { 
+            new Book(1, "Art Of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language")
+        };
         public Book[] GetAllByTitle(string titlePart)
         {
-            throw new NotImplementedException();
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
         }
     }
 }
